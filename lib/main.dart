1bb6ae832c68f0e8a17b4a8e5139bafecfd5a952
linjiansi <pmon.ken.1467@gmail.com>
2023-02-08 00:47:26 +0900

    added screen transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample_app/pokemon_detail.dart';
 
 void main() {
   runApp(const PokemonApp());
@@ -9,46 +10,28 @@ class PokemonApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: PokemonView(),
+      home: TopPage(),
     );
   }
 }
 
-class PokemonView extends StatelessWidget {
-  const PokemonView({Key? key}) : super(key: key);
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-          Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: Image.network(
-                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
-                  height: 100,
-                  width: 100,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  'No.25',
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
-                ),
-              )
-            ],
-          ),
-          const Text(
-            'pikachu',
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-          ),
-          const Chip(label: Text('electric'), backgroundColor: Colors.yellow)
-        ])));
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('detail'),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => const PokemonDetailView())
+            );
+          },
+        )
+      ),
+    );
   }
 }
+
