9a499628473d8210e58650d0ee091114e5486600
linjiansi <pmon.ken.1467@gmail.com>
2023-02-11 12:55:17 +0900

    added api client

---
+++ lib/api/api_client.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_sample_app/api/model/pokemon_list_response.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+part 'api_client.g.dart';
+
+@RestApi(baseUrl: "https://pokeapi.co/api/v2")
+abstract class ApiClient {
+  factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
+
+  @GET("/pokemon")
+  Future<PokemonListResponse> fetchPokemons(
+    @Query("offset") int offset,
+    @Query("limit") int limit,
+  );
+}
