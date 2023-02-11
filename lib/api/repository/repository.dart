066ed2e330990431508c2efc1bf5ebb1866977d2
linjiansi <pmon.ken.1467@gmail.com>
2023-02-11 17:44:56 +0900

    added base repository

---
+++ lib/api/repository/repository.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_sample_app/api/api_client.dart';
+
+class Repository {
+  late ApiClient apiClient;
+
+  Repository() {
+    BaseOptions baseOptions = BaseOptions(
+      contentType: Headers.jsonContentType,
+      responseType: ResponseType.json,
+      receiveTimeout: 30 * 1000,
+
+    );
+    Dio dio = Dio(baseOptions);
+    apiClient = ApiClient(dio);
+  }
+}
