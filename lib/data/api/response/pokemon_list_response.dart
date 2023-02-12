17c5a9b098b251bb1566d19cfeffebd4293f8808
linjiansi <pmon.ken.1467@gmail.com>
2023-02-12 11:44:26 +0900

    fixed pokemon list response

--- lib/data/api/response/pokemon_list_response.dart
+++ lib/data/api/response/pokemon_list_response.dart
@@ -4,14 +4,27 @@ part 'pokemon_list_response.g.dart';
 
 @JsonSerializable()
 class PokemonListResponse {
+  final int count;
+  final String? next;
+  final String? previous;
+  final List<Result> results;
+
+  PokemonListResponse(this.count, this.next, this.previous, this.results);
+
+  factory PokemonListResponse.fromJson(Map<String, dynamic> json) =>
+      _$PokemonListResponseFromJson(json);
+}
+
+@JsonSerializable()
+class Result {
   final String name;
   final String url;
 
-  PokemonListResponse(
+  Result(
     this.name,
     this.url
   );
 
-  factory PokemonListResponse.fromJson(Map<String, dynamic> json) =>
-      _$PokemonListResponseFromJson(json);
+  factory Result.fromJson(Map<String, dynamic> json) =>
+      _$ResultFromJson(json);
 }
--- lib/data/api/response/pokemon_list_response.g.dart
+++ lib/data/api/response/pokemon_list_response.g.dart
@@ -8,13 +8,29 @@ part of 'pokemon_list_response.dart';
 
 PokemonListResponse _$PokemonListResponseFromJson(Map<String, dynamic> json) =>
     PokemonListResponse(
-      json['name'] as String,
-      json['url'] as String,
+      json['count'] as int,
+      json['next'] as String?,
+      json['previous'] as String?,
+      (json['results'] as List<dynamic>)
+          .map((e) => Result.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$PokemonListResponseToJson(
         PokemonListResponse instance) =>
     <String, dynamic>{
+      'count': instance.count,
+      'next': instance.next,
+      'previous': instance.previous,
+      'results': instance.results,
+    };
+
+Result _$ResultFromJson(Map<String, dynamic> json) => Result(
+      json['name'] as String,
+      json['url'] as String,
+    );
+
+Map<String, dynamic> _$ResultToJson(Result instance) => <String, dynamic>{
       'name': instance.name,
       'url': instance.url,
     };
