5eba3549bb19df38ee242e73ac660b24a9b4803e
linjiansi <pmon.ken.1467@gmail.com>
2023-02-12 12:26:07 +0900

    fixed pokemon entity

--- lib/entity/pokemon_list_entity.dart
+++ lib/entity/pokemon_list_entity.dart
@@ -1,8 +1,13 @@
 class PokemonListEntity {
+  final List<PokemonEntity> items;
+  PokemonListEntity(this.items);
+}
+
+class PokemonEntity {
   final String name;
   final String url;
 
-  PokemonListEntity(this.name, this.url);
+  PokemonEntity(this.name, this.url);
 
-  String get id => url.split('/').last;
+  int get id => int.parse( url.split('/').last);
 }
--- lib/mapper/pokemon_list_mapper.dart
+++ lib/mapper/pokemon_list_mapper.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_sample_app/data/api/model/pokemon_list_response.dart';
+import 'package:flutter_sample_app/data/api/response/pokemon_list_response.dart';
 import 'package:flutter_sample_app/entity/pokemon_list_entity.dart';
 import 'package:flutter_sample_app/mapper/mapper.dart';
 
@@ -6,9 +6,8 @@ class PokemonListMapper extends Mapper<PokemonListEntity, PokemonListResponse> {
 
   @override
   PokemonListEntity toEntity(PokemonListResponse response) {
-    return PokemonListEntity(
-      response.name,
-      response.url
-    );
+    return PokemonListEntity(response.results
+        .map((item) => PokemonEntity(item.name, item.url))
+        .toList());
   }
 }
